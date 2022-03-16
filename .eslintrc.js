3aa435aa612014dfb52d1d3d49ec8ceec9e2e220
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-16 16:56:13 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 0,
+    'class-methods-use-this': 0,
+    'import/first': 0,
+  },
+};
